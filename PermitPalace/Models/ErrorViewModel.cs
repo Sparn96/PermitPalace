d6719f259f2ba728bbb53a334c0ad2d47e87efc8
Mitchell Marsh <mitchell@marshhome.net>
2017-10-20 21:53:51 -0700

    Add project files.

---
+++ PermitPalace/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PermitPalace.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
